1d26994a86a66385daf415f57bf895a8c439d0c7
prep0ster0us <rbabu1@unh.newhaven.edu>
2024-03-05 20:00:58 -0500

    Created item model

--- CartCollab/Cart/Models/CartItem.swift
+++ CartCollab/Cart/Models/CartItem.swift
@@ -1,8 +1,23 @@
-//
-//  CartItem.swift
-//  CartCollab
-//
-//  Created by Ritwik on 3/5/24.
-//
-
 import Foundation
+//import Firebase
+
+class CartItem: ObservableObject {
+    let id: String
+    @Published var name: String = ""
+    @Published var type: String = ""
+    @Published var imageID: String = ""
+    
+    required init?(
+        id: String,
+        data: [String: Any]
+    ) {
+        let name = data["name"] as? String != nil ? data["name"] as! String: ""
+        let type = data["type"] as? String != nil ? data["type"] as! String: ""
+        let imageID = data["imageID"] as? String != nil ? data["imageID"] as! String: ""
+        
+        self.id = id
+        self.name = name
+        self.type = type
+        self.imageID = imageID
+    }
+}
